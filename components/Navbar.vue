50ff9fa5148c2ebe02c8980e668bedbf808b668f
Favour Okeibunor <favour@hollatags.com>
2021-05-11 09:50:56 +0100

    add navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <h1>Okeibunor Favour</h1>
+    <ul>
+      <li>About Me</li>
+      <li>My Projects</li>
+      <li>Connect With Me</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,19 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">My Name is Okeibunor Favour. I'm a 10x Dev</h1>
-    </div>
+  <div>
+    <Navbar />
   </div>
 </template>
 
 <script>
-export default {};
+import Navbar from "@/components/Navbar";
+export default {
+  name: "index",
+  components: {
+    Navbar,
+  },
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="sass" scoped>
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
