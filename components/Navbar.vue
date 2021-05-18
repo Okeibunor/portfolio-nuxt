16c377d1adaadd01c2302341741021f4d28e8929
Favour Okeibunor <favour@hollatags.com>
2021-05-18 07:59:55 +0100

    add navbar design

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,17 @@
 <template>
-  <nav>
-    <h1>Okeibunor Favour</h1>
-    <ul>
+  <nav class="navbar">
+    <img src="@/assets/images/logo.svg" alt="" />
+    <ul class="navbar-items">
+      <li class="active">Home</li>
       <li>About Me</li>
-      <li>My Projects</li>
-      <li>Connect With Me</li>
+      <li>Portfolio</li>
+      <li>Resume</li>
+      <li>Mentorship</li>
     </ul>
+    <button class="btn">
+      Talk to Me
+      <img src="@/assets/images/monkeyemoji.svg" alt="" />
+    </button>
   </nav>
 </template>
 
@@ -16,5 +22,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$font-colored: #2d9cdb;
+$button-color: #2d9cdb;
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin: 5vh auto;
+  font-size: 1.5rem;
+}
+.navbar-items {
+  display: flex;
+  width: 45%;
+  justify-content: space-between;
+  align-items: center;
 
+  li {
+    list-style-type: none;
+    &.active {
+      color: $font-colored;
+      &::after {
+        content: "";
+      }
+    }
+  }
+}
+.btn img {
+  vertical-align: middle;
+  margin: auto 0 auto 0.4rem;
+}
 </style>
